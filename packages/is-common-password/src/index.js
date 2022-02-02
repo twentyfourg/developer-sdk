dbc1d7bc6893350f1531808687df47337e356c1f
David Jakowenko <jakowenko@gmail.com>
2022-02-02 15:18:47 -0500

    fix: resolve txt file path relative of the location

--- packages/is-common-password/src/index.js
+++ packages/is-common-password/src/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (password) => {
   const passwords = fs
-    .readFileSync('./src/10-million-password-list-top-1000.txt', 'utf8')
+    .readFileSync(path.resolve(__dirname, './10-million-password-list-top-1000.txt'), 'utf8')
     .split('\n');
   return passwords.includes(password);
 };
