10fd6108ab8edb596f421656efc102107baed7d6
Wyatt <parsonswy@gmail.com>
2022-05-05 13:31:22 -0400

    fix: SQL syntax error when modelName is reserved (#14)
    Co-authored-by: Rose Landroche <50932371+roselandroche@users.noreply.github.com>

--- packages/model-data-generator/src/references.js
+++ packages/model-data-generator/src/references.js
@@ -1,10 +1,14 @@
+const { StringPrompt } = require('enquirer');
 const database = require('./util/db.util');
 const randomByName = require('./randomByName');
 const randomEven = require('./util/randomEven');
 
+const refFields = {};
+
 const references = async (DTO, uniqueObj) => {
   const modelObj = {};
   const errors = [];
+  let modelKey;
 
   for (let i = 0; i < Object.keys(DTO).length; i++) {
     const current = Object.keys(DTO)[i];
@@ -21,7 +25,43 @@ const references = async (DTO, uniqueObj) => {
         const recordType = DTO[current].type.constructor.name;
         let numberType = false;
         modelToQuery = DTO[current].references;
-        const modelName = modelToQuery.model;
+
+        if (!refFields[`${current}`]) {
+          refFields[`${current}`] = '';
+        }
+
+        // all the ways a reference can be defined in Sequelize https://sequelize.org/api/v6/class/src/model.js~model#static-method-init
+        let modelName;
+        if (typeof modelToQuery === 'string') {
+          // references: string
+          modelName = modelToQuery;
+          if (!refFields[`${current}`]) {
+            modelKey = await new StringPrompt({
+              initial: 'id',
+              message: `What key in '${modelName}' model should be referenced in '${current}' column?`,
+            }).run();
+
+            refFields[`${current}`] = modelKey;
+          } else {
+            modelKey = refFields[`${current}`];
+          }
+        } else if (typeof modelToQuery?.model === 'string') {
+          // references: { model: string }
+          modelName = modelToQuery.model;
+          modelKey = modelToQuery.key;
+        } else if (typeof modelToQuery?.model === 'object') {
+          // references: { model: { tableName: string } }
+          // <T extends Sequelize.Model> ends up here too
+          modelName = modelToQuery.model.tableName;
+          modelKey = modelToQuery.key;
+        } else {
+          errors.push(
+            `Model with fields ${Object.keys(
+              DTO
+            )} contains a malformed model reference under key ${current}`
+          );
+          break;
+        }
 
         switch (recordType) {
           case 'INTEGER':
@@ -41,24 +81,20 @@ const references = async (DTO, uniqueObj) => {
         }
         const db = await database.getPool();
         const [record] = await db
-          .query(`SELECT ${modelToQuery.key} FROM ${modelName} ORDER BY RAND() LIMIT 1`)
+          .query(`SELECT ${modelKey} FROM \`${modelName}\` ORDER BY RAND() LIMIT 1`)
           .then(([results]) => results)
           .catch((error) => {
             errors.push(error.message);
-            return [{ [modelToQuery.key]: null }];
+            return [{ [modelToQuery.modelKey]: null }];
           });
         if (Object.keys(DTO[current]) && DTO[current].allowNull) {
-          if (randomEven() || !record[modelToQuery.key]) {
+          if (randomEven() || !record[modelKey]) {
             modelObj[current] = null;
           } else {
-            modelObj[current] = numberType
-              ? Number(record[modelToQuery.key])
-              : String(record[modelToQuery.key]);
+            modelObj[current] = numberType ? Number(record[modelKey]) : String(record[modelKey]);
           }
         } else {
-          modelObj[current] = numberType
-            ? Number(record[modelToQuery.key])
-            : String(record[modelToQuery.key]);
+          modelObj[current] = numberType ? Number(record[modelKey]) : String(record[modelKey]);
         }
       }
     }
